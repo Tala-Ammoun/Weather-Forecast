e566f217d31ed80316986eaf7dcff97ab01d4db7
Tala Ammoun <talaammoun@gmail.com>
2023-01-31 22:15:50 +0000

    weather appears on searching a city, but not when we click a button. Console says that access has been blocked for the URL that I am trying to fetch information from. Managed to save response to local storage, but not get aappend responses to screen upon getting items from storage

--- starter/script.js
+++ starter/script.js
@@ -3,12 +3,22 @@ let key = "&appid=8df441dea99913b147bc9d9c47561bdb";
 let icon = "http://openweathermap.org/img/w/"
 let queryURL
 
+function storedData(){
+localStorage.getItem('response')
+}
+storedData()
+
+
 $(".search-btn").on("click", function () {
   $("#cityName").empty()
   $("#temp").empty();
   $("#wind").empty();
   $("#humidity").empty();
   $("#icon").empty();
+  $(".humidity").empty();
+  $(".wind").empty();
+  $(".temp").empty();
+  $(".icon").empty();
 
   searchInput = $("#search-input").val();
   queryURL = apiURL + "q=" + searchInput + "&limit=1" + key;
@@ -18,36 +28,149 @@ $(".search-btn").on("click", function () {
     .then(response => response.json())
     .then(function (response) {
       console.log(response)
+      localStorage.setItem(response, response)
+
       console.log(response.city.name)
       $("#cityName").append(response.city.name + ": ")
 
       setInterval(function () {
         let today = moment();
-        document.querySelector("#todayDate").textContent = today.format("DD/MM/YYYY");
+        let todayDate = today.format("DD/MM/YYYY")
+        document.querySelector("#todayDate").textContent = todayDate
       })
-
       let temp = response.list[0].main.temp - 273.15 //C = temp – 273.15
       let temperature = Math.round(temp.toFixed(2))
       console.log(temperature + "°C")
       $("#temp").append("Temp: " + temperature + "°C")
-
       console.log(response.list[0].main.humidity + "%")
       $("#wind").append("Wind: " + response.list[0].main.humidity + "%")
-
       console.log(response.list[0].wind.speed + "KPH")
       $("#humidity").append("Humidity: " + response.list[0].wind.speed + "KPH")
-
       let iconURL = icon + response.list[0].weather[0].icon + ".png"
       fetch(iconURL)
         .then(function (result) {
-          let icon = document.createElement("img");
-          icon.src = result.url;
-          $("#icon").append(icon)
-          //$(".icon").append(response.list[0].weather[0].icon)
+          let iconEl = document.createElement("img");
+          iconEl.src = result.url;
+          $("#icon").append(iconEl)
 
+          //5-Day Forecast without a for loop
+          //day 1:
+          setInterval(function () {
+            let today = moment().add(1, 'days');
+            let todayDate = today.format("DD/MM/YYYY")
+            document.querySelectorAll("h5.card-title")[0].textContent = todayDate
+          })
+          let temp1 = response.list[3].main.temp - 273.15 //C = temp – 273.15
+          let temperature1 = Math.round(temp1.toFixed(2))
+          console.log(temperature1 + "°C")
+          $(".temp").eq(0).append("Temp: " + temperature1 + "°C")
+
+          console.log(response.list[3].main.humidity + "%")
+          $(".wind").eq(0).append("Wind: " + response.list[3].main.humidity + "%")
+
+          console.log(response.list[3].wind.speed + "KPH")
+          $(".humidity").eq(0).append("Humidity: " +response.list[3].wind.speed + "KPH")
+
+          let iconURL1 = icon + response.list[3].weather[0].icon + ".png"
+          fetch(iconURL1)
+            .then(function (result) {
+              let iconEl = document.createElement("img");
+              iconEl.src = result.url;
+              $(".icon").eq(0).append(iconEl)
+            })
+
+          //day 2: 
+          setInterval(function () {
+            let today = moment().add(2, 'days');
+            let todayDate = today.format("DD/MM/YYYY")
+            document.querySelectorAll("h5.card-title")[1].textContent = todayDate
+          })
+          let temp2 = response.list[6].main.temp - 273.15 //C = temp – 273.15
+          let temperature2 = Math.round(temp2.toFixed(2))
+          console.log(temperature2 + "°C")
+          $(".temp").eq(1).append("Temp: " + temperature2 + "°C")
+          console.log(response.list[6].main.humidity + "%")
+          $(".wind").eq(1).append("Wind: " + response.list[6].main.humidity + "%")
+          console.log(response.list[6].wind.speed + "KPH")
+          $(".humidity").eq(1).append("Humidity: " +response.list[6].wind.speed + "KPH")
+          let iconURL2 = icon + response.list[6].weather[0].icon + ".png"
+          fetch(iconURL2)
+            .then(function (result) {
+              let iconEl = document.createElement("img");
+              iconEl.src = result.url;
+              $(".icon").eq(1).append(iconEl)
+
+              //day 3:
+              setInterval(function () {
+                let today = moment().add(3, 'days');
+                let todayDate = today.format("DD/MM/YYYY")
+                document.querySelectorAll("h5.card-title")[2].textContent = todayDate
+              })
+              let temp3 = response.list[9].main.temp - 273.15 //C = temp – 273.15
+              let temperature3 = Math.round(temp3.toFixed(2))
+              console.log(temperature3 + "°C")
+              $(".temp").eq(2).append("Temp: " + temperature3 + "°C")
+              console.log(response.list[9].main.humidity + "%")
+              $(".wind").eq(2).append("Wind: " + response.list[9].main.humidity + "%")
+              console.log(response.list[9].wind.speed + "KPH")
+              $(".humidity").eq(2).append("Humidity: " +response.list[9].wind.speed + "KPH")
+              let iconURL3 = icon + response.list[9].weather[0].icon + ".png"
+              fetch(iconURL3)
+                .then(function (result) {
+                  let iconEl = document.createElement("img");
+                  iconEl.src = result.url;
+                  $(".icon").eq(2).append(iconEl)
+
+                  //day 4:
+                  setInterval(function () {
+                    let today = moment().add(4, 'days');
+                    let todayDate = today.format("DD/MM/YYYY")
+                    document.querySelectorAll("h5.card-title")[3].textContent = todayDate
+                  })
+                  let temp4 = response.list[12].main.temp - 273.15 //C = temp – 273.15
+                  let temperature4 = Math.round(temp4.toFixed(2))
+                  console.log(temperature4 + "°C")
+                  $(".temp").eq(3).append("Temp: " + temperature4 + "°C")
+                  console.log(response.list[12].main.humidity + "%")
+                  $(".wind").eq(3).append("Wind: " + response.list[12].main.humidity + "%")
+                  console.log(response.list[12].wind.speed + "KPH")
+                  $(".humidity").eq(3).append("Humidity: " +response.list[12].wind.speed + "KPH")
+                  let iconURL4 = icon + response.list[12].weather[0].icon + ".png"
+                  fetch(iconURL4)
+                    .then(function (result) {
+                      let iconEl = document.createElement("img");
+                      iconEl.src = result.url;
+                      $(".icon").eq(3).append(iconEl)
+
+
+                      //day 5:
+                      setInterval(function () {
+                        let today = moment().add(5, 'days');
+                        let todayDate = today.format("DD/MM/YYYY")
+                        document.querySelectorAll("h5.card-title")[4].textContent = todayDate
+                      })
+                      let temp5 = response.list[15].main.temp - 273.15 //C = temp – 273.15
+                      let temperature5 = Math.round(temp5.toFixed(2))
+                      console.log(temperature5 + "°C")
+                      $(".temp").eq(4).append("Temp: " + temperature4 + "°C")
+                      console.log(response.list[15].main.humidity + "%")
+                      $(".wind").eq(4).append("Wind: " + response.list[15].main.humidity + "%")
+                      console.log(response.list[15].wind.speed + "KPH")
+                      $(".humidity").eq(4).append("Humidity: " +response.list[12].wind.speed + "KPH")
+                      let iconURL5 = icon + response.list[15].weather[0].icon + ".png"
+                      fetch(iconURL5)
+                        .then(function (result) {
+                          let iconEl = document.createElement("img");
+                          iconEl.src = result.url;
+                          $(".icon").eq(4).append(iconEl)
+                        })
+                    })
+                })
+            })
         })
     })
 })
+
 
 Object.keys($(".cityNames")).forEach((element) => {
   if (element < 6) {
@@ -63,52 +186,174 @@ Object.keys($(".cityNames")).forEach((element) => {
     $("#wind").empty();
     $("#humidity").empty();
     $("#icon").empty();
-    
-  fetch(cityURL)
-  .then(response => response.json())
-  .then(function (response) {
-  $("#cityName").append(response.city.name + ": ")
-      setInterval(function () {
-      let today = moment();
-      document.querySelector("#todayDate").textContent = today.format("DD/MM/YYYY");
+    $(".humidity").empty();
+    $(".wind").empty();
+    $(".temp").empty();
+    $(".icon").empty();
+
+    fetch(cityURL)
+      .then(response => response.json())
+      .then(function (response) {
+        $("#cityName").append(response.city.name + ": ")
+        setInterval(function () {
+          let today = moment();
+          document.querySelector("#todayDate").textContent = today.format("DD/MM/YYYY");
         })
 
-      let temp = response.list[0].main.temp - 273.15 //C = temp – 273.15
-      let temperature = Math.round(temp.toFixed(2))
-      console.log(temperature + "°C")
-      $("#temp").append("Temp: " + temperature + "°C")
+        let temp = response.list[0].main.temp - 273.15 //C = temp – 273.15
+        let temperature = Math.round(temp.toFixed(2))
+        console.log(temperature + "°C")
+        $("#temp").append("Temp: " + temperature + "°C")
 
-      console.log(response.list[0].main.humidity + "%")
-      $("#wind").append("Wind: " + response.list[0].main.humidity + "%")
+        console.log(response.list[0].main.humidity + "%")
+        $("#wind").append("Wind: " + response.list[0].main.humidity + "%")
 
-      console.log(response.list[0].wind.speed + "KPH")
-      $("#humidity").append("Humidity: " + response.list[0].wind.speed + "KPH")
+        console.log(response.list[0].wind.speed + "KPH")
+        $("#humidity").append("Humidity: " + response.list[0].wind.speed + "KPH")
 
-      let iconURL = icon + response.list[0].weather[0].icon + ".png"
-      fetch(iconURL)
-      .then(function (result) {
-        let icon = document.createElement("img");
-        icon.src = result.url;
-        $("#icon").append(icon)
+        let iconURL = icon + response.list[0].weather[0].icon + ".png"
+        fetch(iconURL)
+          .then(function (result) {
+            let icon = document.createElement("img");
+            icon.src = result.url;
+            $("#icon").append(icon)
+
+            //5-Day Forecast without a for loop
+            //day 1:
+            setInterval(function () {
+              let today = moment().add(1, 'days');
+              let todayDate = today.format("DD/MM/YYYY")
+              document.querySelectorAll("h5.card-title")[0].textContent = todayDate
+            })
+            let temp1 = response.list[3].main.temp - 273.15 //C = temp – 273.15
+            let temperature1 = Math.round(temp1.toFixed(2))
+            console.log(temperature1 + "°C")
+            $(".temp").eq(0).append("Temp: " + temperature1 + "°C")
+
+            console.log(response.list[3].main.humidity + "%")
+            $(".wind").eq(0).append("Wind: " + response.list[3].main.humidity + "%")
+
+            console.log(response.list[3].wind.speed + "KPH")
+            $(".humidity").eq(0).append("Humidity: " +response.list[3].wind.speed + "KPH")
+
+            let iconURL1 = icon + response.list[3].weather[0].icon + ".png"
+            fetch(iconURL1)
+              .then(function (result) {
+                let iconEl = document.createElement("img");
+                iconEl.src = result.url;
+                $(".icon").eq(0).append(iconEl)
+              })
+
+            //day 2: 
+            setInterval(function () {
+              let today = moment().add(2, 'days');
+              let todayDate = today.format("DD/MM/YYYY")
+              document.querySelectorAll("h5.card-title")[1].textContent = todayDate
+            })
+            let temp2 = response.list[6].main.temp - 273.15 //C = temp – 273.15
+            let temperature2 = Math.round(temp2.toFixed(2))
+            console.log(temperature2 + "°C")
+            $(".temp").eq(1).append("Temp: " + temperature2 + "°C")
+            console.log(response.list[6].main.humidity + "%")
+            $(".wind").eq(1).append("Wind: " + response.list[6].main.humidity + "%")
+            console.log(response.list[6].wind.speed + "KPH")
+            $(".humidity").eq(1).append("Humidity: " +response.list[6].wind.speed + "KPH")
+            let iconURL2 = icon + response.list[6].weather[0].icon + ".png"
+            fetch(iconURL2)
+              .then(function (result) {
+                let iconEl = document.createElement("img");
+                iconEl.src = result.url;
+                $(".icon").eq(1).append(iconEl)
+
+                //day 3:
+                setInterval(function () {
+                  let today = moment().add(3, 'days');
+                  let todayDate = today.format("DD/MM/YYYY")
+                  document.querySelectorAll("h5.card-title")[2].textContent = todayDate
+                })
+                let temp3 = response.list[9].main.temp - 273.15 //C = temp – 273.15
+                let temperature3 = Math.round(temp3.toFixed(2))
+                console.log(temperature3 + "°C")
+                $(".temp").eq(2).append("Temp: " + temperature3 + "°C")
+                console.log(response.list[9].main.humidity + "%")
+                $(".wind").eq(2).append("Wind: " + response.list[9].main.humidity + "%")
+                console.log(response.list[9].wind.speed + "KPH")
+                $(".humidity").eq(2).append("Humidity: " +response.list[9].wind.speed + "KPH")
+                let iconURL3 = icon + response.list[9].weather[0].icon + ".png"
+                fetch(iconURL3)
+                  .then(function (result) {
+                    let iconEl = document.createElement("img");
+                    iconEl.src = result.url;
+                    $(".icon").eq(2).append(iconEl)
+
+                    //day 4:
+                    setInterval(function () {
+                      let today = moment().add(4, 'days');
+                      let todayDate = today.format("DD/MM/YYYY")
+                      document.querySelectorAll("h5.card-title")[3].textContent = todayDate
+                    })
+                    let temp4 = response.list[12].main.temp - 273.15 //C = temp – 273.15
+                    let temperature4 = Math.round(temp4.toFixed(2))
+                    console.log(temperature4 + "°C")
+                    $(".temp").eq(3).append("Temp: " + temperature4 + "°C")
+                    console.log(response.list[12].main.humidity + "%")
+                    $(".wind").eq(3).append("Wind: " + response.list[12].main.humidity + "%")
+                    console.log(response.list[12].wind.speed + "KPH")
+                    $(".humidity").eq(3).append("Humidity: " +response.list[12].wind.speed + "KPH")
+                    let iconURL4 = icon + response.list[12].weather[0].icon + ".png"
+                    fetch(iconURL4)
+                      .then(function (result) {
+                        let iconEl = document.createElement("img");
+                        iconEl.src = result.url;
+                        $(".icon").eq(3).append(iconEl)
+
+
+                        //day 5:
+                        setInterval(function () {
+                          let today = moment().add(5, 'days');
+                          let todayDate = today.format("DD/MM/YYYY")
+                          document.querySelectorAll("h5.card-title")[4].textContent = todayDate
+                        })
+                        let temp5 = response.list[15].main.temp - 273.15 //C = temp – 273.15
+                        let temperature5 = Math.round(temp5.toFixed(2))
+                        console.log(temperature5 + "°C")
+                        $(".temp").eq(4).append("Temp: " + temperature4 + "°C")
+                        console.log(response.list[15].main.humidity + "%")
+                        $(".wind").eq(4).append("Wind: " + response.list[15].main.humidity + "%")
+                        console.log(response.list[15].wind.speed + "KPH")
+                        $(".humidity").eq(4).append("Humidity: " + response.list[12].wind.speed + "KPH")
+                        let iconURL5 = icon + response.list[15].weather[0].icon + ".png"
+                        fetch(iconURL5)
+                          .then(function (result) {
+                            let iconEl = document.createElement("img");
+                            iconEl.src = result.url;
+                            $(".icon").eq(4).append(iconEl)
+                          })
+                      })
+                  })
+              })
+          })
+      })
+  })
 })
-})
-})
-})
 
-//$(".icon").append(response.list[0].weather[0].icon)
-            // let responseArr = response.list
-            //for (let i = 0; i < responseArr.length; i++) {
-            //}
+      // for (let i = 3; i < list.length; i = i + 3) {
+      //   let temp = response.list[i].main.temp - 273.15 //C = temp – 273.15
+      //   let temperature = Math.round(temp.toFixed(2))
+      //   console.log(temperature + "°C")
+      //   $("p:nth-child(1)").append("Temp: " + temperature + "°C")
 
-// setInterval(function () {
-//   let today = moment();
-//   document.querySelector("#todayDate").textContent = today.format("DD/MM/YYYY");
-//   })
+      //   console.log(response.list[i].main.humidity + "%")
+      //   $("p:nth-child(2)").append("Wind: " + response.list[i].main.humidity + "%")
 
+      //   console.log(response.list[i].wind.speed + "KPH")
+      //   $("p:nth-child(3)").append("Humidity: " + response.list[i].wind.speed + "KPH")
 
-//date for next 5 days
-// setInterval(function () {
-//   let today = moment();
-//   for (let i = 0; i < 5; i++) {
-//   document.querySelector(".card-title").textContent[i] = today.format("DD/MM/YYYY" + 1);}
-//   })
+      //   let iconURL = icon + response.list[i].weather[i].icon + ".png"
+      //   fetch(iconURL)
+      //     .then(function (result) {
+      //       let icon = document.createElement("img");
+      //       icon.src = result.url;
+      //       $(".icon").append(icon)
+      //     })
+      // }
